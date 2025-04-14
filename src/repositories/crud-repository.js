8aedaecea1a182788af3ca4d4e5838d77166bb66
Nishith <nishith.one@gmail.com>
2025-04-14 05:45:45 +0530

    Added MVC Boilerplate for booking

---
+++ src/repositories/crud-repository.js
@@ -0,0 +1,50 @@
+const { where } = require("sequelize");
+const AppError = require("../utils/errors/app-error");
+const { StatusCodes } = require("http-status-codes");
+
+class CrudRepository {
+  constructor(model) {
+    this.model = model;
+  }
+
+  async create(data) {
+    const response = await this.model.create(data);
+    return response;
+  }
+
+  async destory(data) {
+    const response = await this.model.destroy({ where: { id: data } });
+    if (!response) {
+      throw new AppError(
+        "Not Able to find the resource",
+        StatusCodes.NOT_FOUND
+      );
+    }
+    return response;
+  }
+
+  async get(data) {
+    const response = await this.model.findByPk(data);
+    if (!response) {
+      throw new AppError(
+        "Not Able to find the resource",
+        StatusCodes.NOT_FOUND
+      );
+    }
+    return response;
+  }
+
+  async getAll() {
+    const response = await this.model.findAll();
+    return response;
+  }
+
+  async update(id, data) {
+    const response = await this.model.update(data, {
+      where: { id: id },
+    });
+    return response;
+  }
+}
+
+module.exports = CrudRepository;
